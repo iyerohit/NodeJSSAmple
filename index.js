eed962cc769eb3eb6f3dc4b744fc96f5603010e0
iyerohit <iyerohit@gmail.com>
2022-07-06 11:32:09 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+
+app.listen(8001);
+console.log("itsworking finally");
+
+app.use("/",(req,res,next)=>{
+
+
+console.log("MIDDLEWARE");
+next();
+});
+
+app.get("/",(req,res)=>{
+    console.log("helllo world");
+    res.send("Hello");
+
+});
+
